9006e4266a534f9792081e14b31ebcba6ac23948
Your Name <you@example.com>
2025-11-10 04:25:35 +0530

    Update server to listen on all interfaces for network access

--- back-end-file/server.js
+++ back-end-file/server.js
@@ -60,7 +60,7 @@ app.get('/health', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
